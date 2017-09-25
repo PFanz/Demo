ccd1a548acc72157b8babc47e5ef694754cca7e8
pengfeifan <pengfeifan@huanqiu.com>
2017-09-25 17:48:41 +0800

    修改格式

--- cors/serve.js
+++ cors/serve.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
 http.createServer((request, response) => {
- response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'})
+  // 允许跨域
+  response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'})
 
   // 发送响应数据 "Hello World"
-  response.end( '{a: "a"}')
+  response.end('{a: "a"}')
 }).listen(8888)
 
 console.log('Server running at http://127.0.0.1:8888/')
